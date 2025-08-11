ed07287517135c2b911e39f36a6b14b4011838b5
Ra-Naun <70940283+Ra-Naun@users.noreply.github.com>
2025-08-11 19:26:17 +0300

    Update relocation.js

--- relocation.js
+++ relocation.js
@@ -1,6 +1,5 @@
 location.href = `
 
-https://viewer.diagrams.net/?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Untitled%20Diagram.drawio&dark=auto#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D12IukJDpgvoZEuZ3LoRQ_06ofcrLRp5x0%26export%3Ddownload
-
+https://viewer.diagrams.net/index.html?tags=%7B%7D&lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Untitled%20Diagram.drawio&dark=true#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D12IukJDpgvoZEuZ3LoRQ_06ofcrLRp5x0%26export%3Ddownload#%7B%22pageId%22%3A%22OjE2b68_DTj0GiLMMWRV%22%7D
 
 `
